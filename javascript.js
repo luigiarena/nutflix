a26fbdbc5946b21eaca2dca1fe26228cc1ee9896
luigiarena <luigiarena.cs@gmail.com>
2022-07-29 21:32:31 +0200

    Eliminazione parte commentata sul file javascript

--- javascript.js
+++ javascript.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
     $("body").css("background", "#555555 !important")
     $(window).scroll(function(){
-/*        var scroll = $(window).scrollTop();
-        if(scroll > 100){
-             $(".nutflix-navbar").css("background", "#0C0C0C")
-        }
-        else {
-            $(".nutflix-navbar").css("background", "transparent")
-        }
-*/
         var scroll = $(window).scrollTop();
         if(scroll > 100){
             $(".nutflix-navbar").addClass("scrolled");
